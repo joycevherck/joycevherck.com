9a826d75251a000b3dc3e3e0f360fdf2bbad3b20
Joyce Van Herck <joyce.vanherck@wearefriday.com>
2019-08-15 14:52:17 +0100

    React set up

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <aside className="sidebar">
+        <div className="sidebarWrapper">
+          <header className="header">Joyce Van Herck</header>
+        </div>
+      </aside>
+      <main className="content">
+        <h2>About</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Pellentesque nec mauris in magna mattis suscipit ac vitae mauris.
+          Quisque pretium erat sed posuere pellentesque. Praesent blandit
+          congue diam, at rhoncus nisl finibus rhoncus. Suspendisse eu ante
+          erat. Orci varius natoque penatibus et magnis dis parturient montes,
+          nascetur ridiculus mus. Nulla eu malesuada ex, at rhoncus mi. Nulla
+          facilisi. Ut placerat, tellus ut hendrerit posuere, orci lectus
+          iaculis tellus, quis sagittis eros ex in metus. Nulla erat ipsum,
+          sollicitudin non elementum tincidunt, faucibus non sapien. Curabitur
+          sollicitudin sollicitudin libero at rhoncus. Nulla sed risus quis
+          quam finibus efficitur eu quis arcu. Donec pretium scelerisque leo
+          vel accumsan.
+        </p>
+        <p>
+          Nullam lobortis justo euismod enim ullamcorper malesuada. Sed
+          dignissim mollis enim a auctor. Nullam porta odio quis est porttitor
+          rutrum. Phasellus vitae facilisis quam. Sed id viverra lorem, vel
+          fringilla arcu. Pellentesque eleifend lorem lacus, ut ultrices odio
+          rhoncus non. Proin egestas justo in lobortis dictum.
+        </p>
+        <p>
+          Vestibulum quam tortor, finibus in euismod ut, hendrerit sed urna.
+          Fusce porta, augue ac faucibus scelerisque, magna mauris porta diam,
+          ac vestibulum est felis at nunc. Vivamus eget sodales purus, non
+          pretium ante. Nunc quis porttitor ipsum. Nunc leo ligula, feugiat ut
+          congue id, pulvinar nec ex. Cras posuere, quam non ornare convallis,
+          lorem magna iaculis ex, ac scelerisque nibh enim vel nulla. Sed non
+          sollicitudin erat. Curabitur nec arcu vel arcu efficitur pharetra.
+          Sed vulputate dolor nec magna molestie, quis tristique dui auctor.
+          Suspendisse turpis orci, fringilla sed turpis ac, venenatis ornare
+          mi. Ut scelerisque erat sit amet orci suscipit porta. Nullam vel
+          urna sit amet massa fermentum porta et sed sapien. Duis finibus nisi
+          ut tellus lacinia, fermentum volutpat leo rutrum. Nulla tincidunt
+          ligula et nulla pellentesque scelerisque. Proin congue arcu quis
+          nibh consectetur hendrerit. Praesent nec velit velit.
+        </p>
+        <p>
+          In luctus magna quis metus pretium fringilla. Sed euismod tortor sit
+          amet augue ornare rhoncus a a arcu. Phasellus nec sodales tortor.
+          Curabitur vulputate finibus dui, sit amet aliquet diam elementum et.
+          Quisque vitae lacus diam. Vestibulum at est arcu. Fusce finibus ut
+          nisi sed iaculis. Suspendisse potenti. Suspendisse potenti. Sed
+          venenatis nisl vel semper posuere. Morbi non euismod metus.
+        </p>
+        <p>
+          Maecenas metus sem, ullamcorper a laoreet ac, feugiat sit amet eros.
+          Ut mattis ex ac augue placerat, ut venenatis ante scelerisque. Sed
+          ullamcorper dolor porttitor, ultrices nulla vehicula, consectetur
+          nibh. Integer malesuada tortor eu orci dignissim pretium. Mauris nec
+          blandit libero. Nunc vestibulum sapien ut molestie vulputate.
+          Praesent vel vulputate mauris. Nunc elit neque, vulputate at lectus
+          id, hendrerit vestibulum nisi. Pellentesque ante nunc, blandit in
+          luctus ac, sodales hendrerit eros. Aenean urna elit, luctus a arcu
+          quis, blandit semper magna. Etiam risus erat, pellentesque ac
+          gravida in, lacinia in arcu. Morbi molestie hendrerit dui. Maecenas
+          commodo, augue vel mattis finibus, est metus pulvinar augue, vitae
+          suscipit urna justo nec risus. In aliquam non ligula a placerat.
+          Quisque leo metus, pulvinar id odio vitae, convallis condimentum
+          dui.
+        </p>
+        <h2>Projects</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Pellentesque nec mauris in magna mattis suscipit ac vitae mauris.
+          Quisque pretium erat sed posuere pellentesque. Praesent blandit
+          congue diam, at rhoncus nisl finibus rhoncus. Suspendisse eu ante
+          erat. Orci varius natoque penatibus et magnis dis parturient montes,
+          nascetur ridiculus mus. Nulla eu malesuada ex, at rhoncus mi. Nulla
+          facilisi. Ut placerat, tellus ut hendrerit posuere, orci lectus
+          iaculis tellus, quis sagittis eros ex in metus. Nulla erat ipsum,
+          sollicitudin non elementum tincidunt, faucibus non sapien. Curabitur
+          sollicitudin sollicitudin libero at rhoncus. Nulla sed risus quis
+          quam finibus efficitur eu quis arcu. Donec pretium scelerisque leo
+          vel accumsan.
+        </p>
+        <p>
+          Nullam lobortis justo euismod enim ullamcorper malesuada. Sed
+          dignissim mollis enim a auctor. Nullam porta odio quis est porttitor
+          rutrum. Phasellus vitae facilisis quam. Sed id viverra lorem, vel
+          fringilla arcu. Pellentesque eleifend lorem lacus, ut ultrices odio
+          rhoncus non. Proin egestas justo in lobortis dictum.
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
